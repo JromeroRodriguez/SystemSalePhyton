4b535ae8673c79ca01c6a9a60a49f83c908067e7
JromeroRodriguez <josephteck123@gmail.com>
2026-03-11 07:14:42 -0500

    Update author and minor fixe

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ if __name__ == "__main__":
     # Register daily sales
     sales, grand_total = register_sales()
     
-    
+
     # Show sales summary
     show_summary(sales, grand_total)
+
+print("Thank you for using the sales registration system!")
